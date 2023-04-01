c4a54542b5897efa0ef1f860c7b4f3ee553a63a4
Samanta Podgórska <117597986+Samcia@users.noreply.github.com>
2023-04-01 11:27:55 +0200

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+for (const img of galleryItems) {
+  const listEl = `<div class="gallery__item">
+    <a class="gallery__link" href=${img.original}>
+      <img
+        class="gallery__image"
+        src=${img.preview}
+        data-source=${img.original}
+        alt=${img.description}
+      />
+    </a>
+  </div>`;
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+}
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const box = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  box.show();
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    window.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+for (const img of galleryItems) {
+  const listEl = `<li>
+  <a class="gallery__item" href=${img.original}>
+    <img class="gallery__image" src=${img.preview} 
+    alt=${img.description} />
+  </a></li>`;
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+console.log(galleryEl);
